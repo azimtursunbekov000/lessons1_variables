87e7336d6837fa3aec3f6d457158ae012fcbd06e
azim <azimtursunbekov03@gmail.com>
2023-09-04 13:01:28 +0600

    практические задание N3

---
+++ lesson3Operationtypes.dart
@@ -0,0 +1,54 @@
+void main() {
+  var a = 1;
+  var b = 2;
+  // a += b;
+  var c = "";
+
+  bool res = a > b || c.isEmpty;
+  print(res);
+
+// compareTo() - Он сравнивает значение с другим числом.
+  var a1 = 12;
+  var b2 = 2;
+
+  int res2 = a1.compareTo(b2);
+  print(res2);
+// toInt() - Возвращает целочисленное эквивалентное представление числа.
+  double a3 = 1.2;
+  var b3 = 2;
+  int res3 = a3.toInt();
+  print(res3);
+// Возвращает округление числа.
+  double a4 = 1.6;
+  var b4 = 2;
+  int res4 = a4.round();
+  print("${res4} res4");
+// num.parse() - преобразование числовые строки в числа
+  String a5 = "2fff";
+  var b5 = 2;
+  int? res5 = int.tryParse(a5);
+  print(a5);
+
+  //isEmpty – Если строка пуста, возвращается истина.
+  String a6 = "dddd";
+  print(a6.isEmpty);
+
+  //Length - Он возвращает длину строки, включая пробелы.
+  String a7 = "Azimu T";
+  print(a7.length);
+// конвертация с строку и посчитали длину
+  String a8 = "2fff";
+  var b8 = 2222;
+  int count = b8.toString().length;
+  print(count);
+
+  // list
+  var list1 = [10, 15, 20, 25, 25];
+  list1.add(a);
+  print(list1.reversed);
+
+  // цикл фор
+  for (int i = 0; i < 10; i++) {
+    print(i);
+  }
+}
