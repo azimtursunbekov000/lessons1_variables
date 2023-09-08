70a9da3dce144c06e48f6ad682df8339d04cd71e
azim <azimtursunbekov03@gmail.com>
2023-09-08 17:44:57 +0600

    Lessons5 practice

--- HomeWork/lessons5.dart
+++ HomeWork/lessons5.dart
@@ -1,3 +1,138 @@
+import 'dart:io';
+
 void main(List<String> args) {
-  //
+  //1.Если переменная a равна 10, то выведите &#39;Верно&#39;, иначе
+// выведите &#39;Неверно&#39;.
+  int a = 10;
+  switch (a) {
+    case 1:
+    case 2:
+    case 23:
+      print('Неверно');
+      break;
+    case 10:
+    case 7:
+    case 26:
+      print("Верно");
+      break;
+    default:
+      print("нету этого числа");
+  }
+
+//  2 Переменная lang может принимать 2 значения: ru en. Если она
+// имеет значение ru, то в переменную arr запишем массив дней
+// недели на русском языке, а если имеет значение en – то на
+// английском. Решите задачу через 2 if, через switch-case.
+  var lang = 'en';
+  var arr;
+
+  if (lang == 'ru') {
+    arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+  } else if (lang == 'en') {
+    // с else, второе условие не будет лишний раз проверяться, если первое выполнилось
+    arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+  }
+  print(arr);
+
+  // 3Переменная num может принимать 4 значения: 1, 2, 3 или 4.
+// Если она имеет значение &#39;1&#39;, то в переменную result запишем &#39;зима&#39;,
+// если имеет значение &#39;2&#39; – &#39;весна&#39; и так далее. Решите задачу через
+// switch-case.
+
+  var num = 1;
+  String res;
+  switch (num) {
+    case 1:
+    case 2:
+    case 12:
+      res = "Зима";
+      break;
+    case 3:
+    case 4:
+    case 5:
+      res = "Весна";
+      break;
+    case 6:
+    case 7:
+    case 8:
+      res = "Лето";
+      break;
+    case 9:
+    case 10:
+    case 11:
+      res = "Осень";
+      break;
+    default:
+      res = "Нет такого месяца!";
+  }
+  print(res);
+
+  // 4
+  print("Введите какой день недели");
+  int? day = int.parse(stdin.readLineSync() ?? " ");
+  String resultat;
+  switch (day) {
+    case 1:
+      resultat = "понедельник  \nу вас 2 пары";
+      break;
+    case 2:
+      resultat = "вторник \nВы сегодня свободны ";
+      break;
+    case 3:
+      resultat = "среда \nу вас 3 пары";
+      break;
+    case 4:
+      resultat = "четверг \nу вас 4 пары";
+      break;
+    case 5:
+      resultat = "пятница \nу вас 1 пара";
+      break;
+    case 6:
+      resultat = "суббота \nВыходной";
+      break;
+    case 7:
+      resultat = "воскресенье \nТоже чилим";
+      break;
+    default:
+      resultat = ("нету такого дня");
+  }
+  print(resultat);
+
+  // while.1 practice Выведите столбец чисел от 1 до 100.
+  var c = 100;
+  int i = 1;
+  while (i <= c) {
+    print(i);
+    i++;
+  }
+  // 2 Выведите столбец чисел от 11 до 33.
+  var c1 = 33;
+  int i1 = 11;
+  while (i1 <= c1) {
+    print("\n $i1");
+    i1++;
+  }
+
+  // 3Выведите столбец четных чисел в промежутке от 0 до 100.
+  int i2 = 1;
+
+  while (i2 <= 100) {
+    i2++;
+
+    if (i2 % 2 == 0) {
+      print(i2);
+    }
+  }
+  // 4
+  var sum = 0;
+  int i3 = 0;
+
+  // for (var i = 1; i <= 100; i++) {
+  //   sum += i;
+  // }
+  while (i3 < 100) {
+    i3++;
+    sum += i3;
+    print(sum);
+  }
 }
--- lesson4.dart
+++ lesson4.dart
@@ -1,58 +1,58 @@
 import 'dart:io';
 
 void main(List<String> args) {
-// //   // practice1
+// // //   // practice1
 //   int a = 5;
 //   double b = 5.126;
 //   var c = a + b;
 //   print("${a} + ${b} = $c");
-// //   // 2
-// //   // practice
+// // //   // 2
+// // //   // practice
 //   int a1 = 5;
 //   int b1 = 2;
 //   int c1 = a1 + b1;
 //   print("${a1} + ${b1} = $c1");
-// //   // practice1
+// // //   // practice1
 //   int a2 = 5;
 //   int b2 = 2;
 //   int c2 = a2 - b2;
 //   print("${a2} - ${b2} = $c2");
-// //   // practice2
+// // //   // practice2
 //   int a3 = 132;
 //   int b3 = 123;
 //   int c3 = a3 * b3;
 //   print("${a3} * ${b3} = $c3");
-// //   // practice3
+// // //   // practice3
 //   int a4 = 78;
 //   int b4 = 2;
 //   var c4 = a4 ~/ b4;
 //   print("${a4} ÷ ${b4} = $c4");
-// //   // practice4
+// // //   // practice4
 //   int a5 = 78;
 //   var b5 = 2.5;
 //   var c5 = a5 ~/ b5;
 //   print("${a5} ÷ ${b5} = $c5");
-// //   // practice5
+// // //   // practice5
 //   int a6 = 0;
 //   var b6 = 5;
 //   var c6 = a6 * b6;
 //   print("${a6} * ${b6} = $c6");
 
-// //   // 3
-// //   // practice1
+// // //   // 3
+// // //   // practice1
 //   int a7 = 4;
 //   int b7 = 5;
 //   print("a7 = ${a7} -----> b7 = ${b7} ");
-// //   // 4+5
+// // //   // 4+5
 //   a7 += b7;
-// //   //  = 9-4
+// // //   //  = 9-4
 //   b7 = a7 - b7;
-// //   // 9 -5
+// // //   // 9 -5
 //   a7 -= b7;
 //   print("a7= ${a7} <----- b7 = ${b7}");
 
-// //   // 4
-// //   // practice1
+// // //   // 4
+// // //   // practice1
 
 //   print("Введите ваше имя:");
 
@@ -62,22 +62,22 @@ void main(List<String> args) {
 
 //   print("Ваше имя $userAnswer, Ваш возраст $userAge");
 
-// // //   // 5) Создайте программу, которая просит у пользователя номер. В зависимости от того,
-// // // подходит номер по длине (является валидным), распечатайте соответствующее
-// // // сообщение для пользователя.
+// // // //   // 5) Создайте программу, которая просит у пользователя номер. В зависимости от того,
+// // // // подходит номер по длине (является валидным), распечатайте соответствующее
+// // // // сообщение для пользователя.
 
-//   // print("Введите ваш номер телефона");
-//   // String? userPhoneNumber = stdin.readLineSync(); //996550567779
+//   print("Введите ваш номер телефона");
+//   String? userPhoneNumber = stdin.readLineSync(); //996550567779
 
-//   // if (userPhoneNumber.length == 12) {
-//   //   print("успешно");
-//   // } else {
-//   //   print("провалено");
-//   // }
+//   if (userPhoneNumber?.length != 12) {
+//     print("провалено");
+//   } else {
+//     print("успешно");
+//   }
 
-// //   // 6Создайте программу, которая в зависимости от текущей погоды подсказывает вам
-// // брать зонт или нет. (Подсказка: Нужно создать переменную bool и присвоить ей значение,
-// // сделать с помощью тернарного оператора (? : )). НЕ СМОГ СДЕЛАТЬ ЧЕРЕЗ ТЕРНАРНЫЙ ОПЕРАТОР!!!!!!
+// // //   // 6Создайте программу, которая в зависимости от текущей погоды подсказывает вам
+// // // брать зонт или нет. (Подсказка: Нужно создать переменную bool и присвоить ей значение,
+// // // сделать с помощью тернарного оператора (? : )). НЕ СМОГ СДЕЛАТЬ ЧЕРЕЗ ТЕРНАРНЫЙ ОПЕРАТОР!!!!!!
 //   print("Пожалуста, подскажите какая погода  сегодня");
 //   String? pogoda = stdin.readLineSync();
 
@@ -88,18 +88,17 @@ void main(List<String> args) {
 //     print("Зонт вам не нужен");
 //   }
 
-//   // 7Дано число. Если оно больше 3, то увеличить число на 10, иначе уменьшить на 10.
+// //   // 7Дано число. Если оно больше 3, то увеличить число на 10, иначе уменьшить на 10.
 //   print('Введите число:');
 //   var x = int.parse(stdin.readLineSync() ?? '0');
 //   if (x > 3) {
 //     print('Результат X + Y = ${x + 10}');
 //   } else {
-//     x < 3;
 //     print('Результат X - Y = ${x - 10}');
 //   }
 
-//   // 8 Дано число. Если оно меньше 7, то вывести Yes, если больше 10, то вывести No, если
-// // равно 9, то вывести Error.
+// //   // 8 Дано число. Если оно меньше 7, то вывести Yes, если больше 10, то вывести No, если
+// // // равно 9, то вывести Error.
 //   print('Введите число:');
 //   var y = int.parse(stdin.readLineSync() ?? '0');
 //   if (y < 7) {
@@ -112,91 +111,97 @@ void main(List<String> args) {
 //     print("Error");
 //   }
 
-// 9 Даны два числа. Вывести наибольшее из них.
-  // print("Введите число1");
-  // var x1 = int.parse(stdin.readLineSync() ?? '0');
-  // print("Введите число2");
-  // var y1 = int.parse(stdin.readLineSync() ?? '0');
-  // if (x1 > y1) {
-  //   print("\nmax is x=$x1");
-  // } else {
-  //   print("\nmax is y=$y1");
-  // }
+// // 9 Даны два числа. Вывести наибольшее из них.
+//   print("Введите число1");
+//   var x1 = int.parse(stdin.readLineSync() ?? '0');
+//   print("Введите число2");
+//   var y1 = int.parse(stdin.readLineSync() ?? '0');
+//   if (x1 > y1) {
+//     print("\nmax is x=$x1");
+//   } else {
+//     print("\nmax is y=$y1");
+//   }
 
-  // 10 Даны два числа. Вывести yes, если их сумма равна 100, иначе вывести No.
-  // print("Введите число1");
-  // var x2 = int.parse(stdin.readLineSync() ?? '0');
-  // print("Введите число2");
-  // var y2 = int.parse(stdin.readLineSync() ?? '0');
-  // if (x2 + y2 == 100) {
-  //   print("Yes");
-  // } else {
-  //   print("No");
-  // }
+//   // 10 Даны два числа. Вывести yes, если их сумма равна 100, иначе вывести No.
+//   // print("Введите число1");
+//   var x2 = int.parse(stdin.readLineSync() ?? '0');
+//   print("Введите число2");
+//   var y2 = int.parse(stdin.readLineSync() ?? '0');
+//   if (x2 + y2 == 100) {
+//     print("Yes");
+//   } else {
+//     print("No");
+//   }
 
-  // 11 Пользователь вводит номер месяца. Вывести время года(весна, зима, лето)
-  // print("Введите номер месяца");
-  // var month = int.parse(stdin.readLineSync() ?? "0");
-  // String res;
-  // switch (month) {
-  //   case 1:
-  //   case 2:
-  //   case 12:
-  //     res = "Зима";
-  //     break;
-  //   case 3:
-  //   case 4:
-  //   case 5:
-  //     res = "Весна";
-  //     break;
-  //   case 6:
-  //   case 7:
-  //   case 8:
-  //     res = "Лето";
-  //     break;
-  //   case 9:
-  //   case 10:
-  //   case 11:
-  //     res = "Осень";
-  //     break;
-  //   default:
-  //     res = "Нет такого месяца!";
-  // }
-  // print(res);
+//   // 11 Пользователь вводит номер месяца. Вывести время года(весна, зима, лето)
+//   // print("Введите номер месяца");
+//   var month = int.parse(stdin.readLineSync() ?? "0");
+//   String res;
+//   switch (month) {
+//     case 1:
+//     case 2:
+//     case 12:
+//       res = "Зима";
+//       break;
+//     case 3:
+//     case 4:
+//     case 5:
+//       res = "Весна";
+//       break;
+//     case 6:
+//     case 7:
+//     case 8:
+//       res = "Лето";
+//       break;
+//     case 9:
+//     case 10:
+//     case 11:
+//       res = "Осень";
+//       break;
+//     default:
+//       res = "Нет такого месяца!";
+//   }
+//   print(res);
 
-  // 12 Даны три числа. Вывести на экран наименьшее из них.
-  // print("Ввудите а");
-  // var a11 = int.parse(stdin.readLineSync() ?? "0");
-  // print("Ввудите b");
-  // var b11 = int.parse(stdin.readLineSync() ?? "0");
-  // print("Ввудите c");
-  // var c11 = int.parse(stdin.readLineSync() ?? "0");
+//   // 12 Даны три числа. Вывести на экран наименьшее из них.
+//   print("Ввудите а");
+//   var a11 = int.parse(stdin.readLineSync() ?? "0");
+//   print("Ввудите b");
+//   var b11 = int.parse(stdin.readLineSync() ?? "0");
+//   print("Ввудите c");
+//   var c11 = int.parse(stdin.readLineSync() ?? "0");
 
-  // if (a11 < b11) {
-  //   if (a11 < c11) {
-  //     print("min $a11");
-  //   }
-  // } else {
-  //   print("min $c11");
-  // }
-  // if (b11 < c11) {
-  //   print("min $b11");
-  // } else {
-  //   print("min $c11");
-  // }
+//   if (a11 < b11) {
+//     if (a11 < c11) {
+//       print("min $a11");
+//     }
+//   } else {
+//     print("min $c11");
+//   }
+//   if (b11 < c11) {
+//     print("min $b11");
+//   } else {
+//     print("min $c11");
+//   }
 
   // 13 Дано трехзначное число. Переставьте первую и последнюю цифры.
-  // int a22 = 123;
+  // int a22 = 321;
   // String b22 = a22.toString();
   // List c22 = b22.split("");
   // c22.insert(0, c22.last);
-  // c22.insert(3, c22[3]);
-  // c22.removeAt(3);
+  // c22.insert(3, c22[1]);
+  // c22.removeAt(1);
   // c22.removeAt(c22.length - 1);
   // print(c22);
-
-  // 16
-  var lines = 7;
+  // // 14
+  // List a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  // for (int i = 0; i < a.length; i++) {
+  //   if (a[i] < 5) {
+  //     print(a[i]);
+  //   }
+  // }
+  // 15
+  var lines = 10;
   var str = " ";
   var star = "*";
   for (var i = 0; i < lines; i++) {
